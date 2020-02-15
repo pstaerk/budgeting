c7ca40071d2866df75bcc1caf5e43d65c2a8991b
FranzDuck <franz-duck@gmx.de>
2020-02-15 21:16:04 +0100

    Simple class representing a expense

---
+++ budgeting/expense.py
@@ -0,0 +1,7 @@
+class expense:
+    """A class representing a expense"""
+    def __init__(self, name, category, price, note):
+        self._name = name
+        self._price
+        self._category = category
+        self._note = note
