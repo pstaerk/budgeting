e0e5563236792c9251f01cb5adf7d44ccf9b3e69
FranzDuck <franz-duck@gmx.de>
2020-02-16 14:54:24 +0100

    Summing functionality

--- budgeting/expense.py
+++ budgeting/expense.py
@@ -2,6 +2,18 @@ class expense:
     """A class representing a expense"""
     def __init__(self, name, category, price, note):
         self._name = name
-        self._price
+        self._price = float(price)
         self._category = category
         self._note = note
+    
+    def __add__(self, x):
+        if type(x) == type(self):
+            return self._price + x._price
+        else:
+            return self._price + x
+        
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        else:
+            return self.__add__(other)
