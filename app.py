849ecca53ab4ba7c70efa773b1760908dd4cc6ce
FranzDuck <franz-duck@gmx.de>
2020-02-16 14:53:18 +0100

    Minimum working example of loading and saving data

--- app.py
+++ app.py
@@ -6,27 +6,26 @@ track of expenses, without much clutter.
 
 from ui import simplegui as ui
 from dat_handling import io
-from budgeting import expense
+from budgeting import expense as ex
 
 def main():
-    """Starting the main app.
+    """Starting the main (gui) app.
 
     """
     # Load the data from the database.
     known_categories = io.load_list_from_txt()
-
-    # For testing only:
-    test_e = e.expense('Drone', 'Misc.', 192.3, 'Test note')
+    expenses         = io.load_expenses()
 
     # Pre-Create the specific "menus"
-    new_it = ui.BudgetMenu(known_categories=known_categories)
-    main   = ui.MainMenu(budget_m, [test_e])
+    budget_m = ui.BudgetMenu(known_categories=known_categories)
+    main     = ui.MainMenu(budget_m, expenses)
 
     # Display some sort of menu to the user.
-    new_exp = new_it.new_budget_item()
+    main.display()
 
-    # Wait for input until finished
-    # Save to database.
+    # Save menu entries to database.
+    io.save_budget(main._expenses)
+    io.save_categories(budget_m._known_categories)
 
 if __name__ == "__main__":
     main()
